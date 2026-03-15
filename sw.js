9ed8e28774392c88418d0a7fcd41202c2086ce0e
chaosek <87547081+chaosek@users.noreply.github.com>
2026-03-15 01:28:23 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // GPS Stopař – Service Worker
 // Verzi změň kdykoli chceš vynutit refresh cache
-const CACHE_VERSION = 'gps-stopar-202603150007';
+const CACHE_VERSION = 'gps-stopar-202603150026';
 
 const APP_SHELL = [
   './index.html',
